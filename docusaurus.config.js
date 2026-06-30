c9e3cdab0dd7c2fc80ede7dd85c7746c60f6d65c
Karol Rosa <144715554+ksrosa1@users.noreply.github.com>
2026-06-30 13:24:21 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://inop.plvacc.pl',
+  url: 'https://om.plvacc.pl',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
